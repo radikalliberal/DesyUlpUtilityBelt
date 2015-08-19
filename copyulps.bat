88ce43e06fd01bcac1bf8bddeb7c0831ee9ea465
unknown <scholzj@SZEPCX16631.desy.de>
2015-08-19 16:36:10 +0200

    Signed-off-by: unknown <scholzj@SZEPCX16631.desy.de>

---
+++ copyulps.bat
@@ -0,0 +1,11 @@
+Copy "H:\sze\Eagle6\DesyUlpUtilityBelt\LP-Daten.ulp" "N:\4all\public\Eagle_6+\ulp\LP-Daten.ulp" 
+Copy "H:\sze\Eagle6\DesyUlpUtilityBelt\bom.ulp" "N:\4all\public\Eagle_6+\ulp\bom.ulp"
+Copy "H:\sze\Eagle6\DesyUlpUtilityBelt\PlatzhalterErsetzen.ulp" "N:\4all\public\Eagle_6+\ulp\PlatzhalterErsetzen.ulp"
+Copy "H:\sze\Eagle6\DesyUlpUtilityBelt\Libdesy.lib" "N:\4all\public\Eagle_6+\ulp\Libdesy.lib"
+Copy "H:\sze\Eagle6\DesyUlpUtilityBelt\Libdesy.lib" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\Libdesy.lib"
+Copy "H:\sze\Eagle6\DesyUlpUtilityBelt\readlibs.ulp" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\readlibs.ulp"
+Copy "H:\sze\Eagle6\DesyUlpUtilityBelt\editbau.ulp" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\editbau.ulp"
+Copy "H:\sze\Eagle6\DesyUlpUtilityBelt\nextbib.ulp" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\nextbib.ulp"
+Copy "H:\sze\Eagle6\DesyUlpUtilityBelt\tags.ulp" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\tags.ulp"
+
+pause
