3790c87de9ad4d26b8a9021dfd81ad52b7bdbf4d
Scholz <scholzj@win.desy.de>
2016-02-11 18:21:05 +0100

    script zum synchronisieren der Dateien Lokal/Netz

--- copyulps.bat
+++ copyulps.bat
@@ -1,11 +1,24 @@
-Copy "LP-Daten.ulp" "N:\4all\public\Eagle_6+\ulp\LP-Daten.ulp" 
-Copy "bom.ulp" "N:\4all\public\Eagle_6+\ulp\bom.ulp"
-Copy "PlatzhalterErsetzen.ulp" "N:\4all\public\Eagle_6+\ulp\PlatzhalterErsetzen.ulp"
-Copy "Libdesy.lib" "N:\4all\public\Eagle_6+\ulp\Libdesy.lib"
-Copy "Libdesy.lib" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\Libdesy.lib"
-Copy "readlibs.ulp" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\readlibs.ulp"
-Copy "editbau.ulp" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\editbau.ulp"
-Copy "nextbib.ulp" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\nextbib.ulp"
-Copy "tags.ulp" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\tags.ulp"
+SET FILES=LP-Daten.ulp bom.ulp PlatzhalterErsetzen.ulp Libdesy.lib Libdesy.lib readlibs.ulp editbau.ulp nextbib.ulp tags.ulp
+SET ULPP='N:\4all\public\Eagle_6+\ulp'
+FOR %%d IN (%FILES%) DO (
+	FOR %%i IN (%%d) DO SET DATE1=%%~ti
+	FOR %%i IN (%ULPP%%%d) DO SET DATE2=%%~ti
+	IF "%DATE1%"=="%DATE2%" ECHO Files have same age && GOTO END
+	::FOR /F %%i IN ('DIR /B /O:D %%d %ULP%%%d') DO SET NEWEST=%%i
+	::ECHO Newer file is %NEWEST%
+
+	:END
+
+)
+
+::Copy "LP-Daten.ulp" "N:\4all\public\Eagle_6+\ulp\LP-Daten.ulp" 
+::Copy "bom.ulp" "N:\4all\public\Eagle_6+\ulp\bom.ulp"
+::Copy "PlatzhalterErsetzen.ulp" "N:\4all\public\Eagle_6+\ulp\PlatzhalterErsetzen.ulp"
+::Copy "Libdesy.lib" "N:\4all\public\Eagle_6+\ulp\Libdesy.lib"
+::Copy "Libdesy.lib" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\Libdesy.lib"
+::Copy "readlibs.ulp" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\readlibs.ulp"
+::Copy "editbau.ulp" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\editbau.ulp"
+::Copy "nextbib.ulp" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\nextbib.ulp"
+::Copy "tags.ulp" "N:\4all\public\Eagle_6+\ulp\Ulp-Service\tags.ulp"
 
 pause
---
+++ copyulps.ps1
@@ -0,0 +1,27 @@
+﻿$LocalBaseDir = "N:\scholzj\sze\DesyUlpUtilityBelt\"
+$EagleBaseDir = "N:\4all\public\Eagle_6+\ulp\"
+
+$files = @("Libdesy.lib","bom.ulp","Ulp-Service\editbau.ulp","Ulp-Service\readlibs.ulp","Ulp-Service\nextbib.ulp","Ulp-Service\tags.ulp","Ulp-Service\uebersicht.ulp")
+
+foreach ($item in $files) {
+
+$LocalSource = $LocalBaseDir + $item
+$EagleSource = $EagleBaseDir + $item
+
+$LocalFileDate = (ls $LocalSource).LastWriteTime
+$EagleFileDate = (ls $EagleSource).LastWriteTime
+
+#echo "$LocalSource :$LocalFileDate"
+#echo "$EagleSource :$EagleFileDate"
+
+	if ($LocalFileDate -lt $EagleFileDate) {
+		Copy-Item  $EagleSource -Destination $LocalSource -force
+		echo "Local file$LocalSource was older and has been replaces with $EagleSource"
+	}
+	if ($LocalFileDate -gt $EagleFileDate) {
+		Copy-Item  $LocalSource -Destination $EagleSource -force
+		echo "External file $EagleSource was older and has been replaces with $LocalSource"
+	}
+}
+
+
